d24b6f83032824fc27e9d96f2aea63c4e8e3b586
shuto0410 <69708307+shuto0410@users.noreply.github.com>
2021-07-27 16:35:38 +0900

    ダミー広島まで全部入れたよ

--- posse-3rd-laravel-study/database/seeds/QuestionsTableSeeder.php
+++ posse-3rd-laravel-study/database/seeds/QuestionsTableSeeder.php
@@ -36,6 +36,102 @@ class QuestionsTableSeeder extends Seeder
                 'img_name'=> "1-4.png",
                 'commentary'=> "流石にこの漢字は読めると思います。",
             ],
+            [
+                'title_id' => 1,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "1-5.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 1,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "1-6.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 1,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "1-7.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 1,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "1-8.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 1,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "1-9.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 1,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "1-10.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-1.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-2.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-3.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-4.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-5.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-6.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-7.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-8.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-9.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
+            [
+                'title_id' => 2,
+                'sentence'=> "この漢字はなんて読む？",
+                'img_name'=> "2-10.png",
+                'commentary'=> "流石にこの漢字は読めると思います。",
+            ],
         ]);
     }
 }
