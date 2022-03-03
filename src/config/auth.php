6907a925781db52dd58393608ff54b97d59afe06
shuto0410 <69708307+shuto0410@users.noreply.github.com>
2022-03-03 22:18:47 +0900

    laravel初期化

--- src/config/auth.php
+++ src/config/auth.php
@@ -31,7 +31,7 @@ return [
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
-    | Supported: "session", "token"
+    | Supported: "session"
     |
     */
 
@@ -39,17 +39,6 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
-        ],
-
-        'admin' => [
-            'driver' => 'session',
-            'provider' => 'admin',
-        ],
-
-        'api' => [
-            'driver' => 'token',
-            'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
@@ -69,16 +58,17 @@ return [
     | Supported: "database", "eloquent"
     |
     */
+
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
-        
-        'admin' => [
-            'driver' => 'eloquent',
-            'model' => App\Admin::class,
-        ],
+
+        // 'users' => [
+        //     'driver' => 'database',
+        //     'table' => 'users',
+        // ],
     ],
 
     /*
@@ -90,7 +80,7 @@ return [
     | than one user table or model in the application and you want to have
     | separate password reset settings based on the specific user types.
     |
-    | The expire time is the number of minutes that the reset token should be
+    | The expire time is the number of minutes that each reset token will be
     | considered valid. This security feature keeps tokens short-lived so
     | they have less time to be guessed. You may change this as needed.
     |
@@ -99,12 +89,6 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_resets',
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-        'admin' => [
-            'provider' => 'admin',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,33 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-// ログイン
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-// パスワードリセット
-Route::get('password/update', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.update');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/update', 'Auth\ResetPasswordController@reset');
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-
-Route::group(['middleware' => 'login_check'],function () {
-    Route::get('/', 'Top\TopController@index')->name('top');;
-    Route::get('/get_event/{eventId}', 'Top\TopController@sendData');
-    Route::post('/send', 'Top\TopController@create');
-
-    Route::group(['middleware' => 'is_admin_role'], function () {
-        Route::get('/admin/add-user', 'Auth\RegisterController@showRegistrationForm')->name('register');
-        Route::post('/admin/add-user', 'Auth\RegisterController@register');
-        Route::get('/admin/add-event','Admin\AddEventController@index')->name('add_event');
-        Route::post('/admin/add-event','Admin\AddEventController@add_event');
-        Route::get('/admin/edit-event/{eventId}','Admin\EditEventController@edit')->name('event_edit');
-        Route::get('/admin/delete-event/{eventId}','Admin\EditEventController@delete')->name('event_delete');
-        Route::get('/admin/list-event','Admin\ListEventController@index')->name('list_event');
-        Route::get('/admin/detail-event','Admin\DetailEventController@index')->name('detail_event');
-    });
-
+Route::get('/', function () {
+    return view('welcome');
 });
